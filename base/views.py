57734db43d5efa1e116ddfba6a527fabb24ca64f
Mista Log <oloyedeoibrahimsmile@gmail.com>
2025-02-23 16:22:37 +0100

    Configured signup page

--- base/views.py
+++ base/views.py
@@ -1,10 +1,67 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+
 
 # Create your views here.
 def registerPage(request):
+    if request.method == "POST":
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        password1 = request.POST.get("password1")
+        password2 = request.POST.get("password2")
+
+        if password1 != password2:
+            messages.error(request, "Passwords do not match!")
+            return redirect("register")
+
+        if User.objects.filter(username=first_name).exists():
+            messages.error(request, "Username already taken!")
+            return redirect("signup")
+
+        user = User.objects.create_user(username=first_name, first_name = first_name, last_name = last_name, email=email, password=password1)
+        user.save()
+        messages.success(request, "Account created successfully. You can now log in.")
+        return redirect("login")
     return render(request, 'register.html')
 
+
+    # def loginPage(request):
+    #     if request.method == "POST":
+    #         username = request.POST.get("username")
+    #         password = request.POST.get("password")
+
+    #         user = auth.authenticate(request, username=username, password=password)
+    #         if user is not None:
+    #             auth.login(request, user)
+    #             messages.success(request, "You are now logged in.")
+    #             return redirect("home")
+    #         else:
+    #             messages.error(request, "Invalid username or password.")
+    #             return redirect("login")
+    #     return render(request, 'login.html')
+
+
 def loginPage(request):
+    # if request.method == "POST":
+    #     email = request.POST.get("email")
+    #     password = request.POST.get("password")
+
+    #     user = auth.authenticate(email=email, password=password)
+    #     if user is not None:
+    #         auth.login(request, user)
+    #         messages.success(request, "You are now logged in.")
+    #         return redirect("home")
+    #     else:
+    #         messages.error(request, "Invalid email or password.")
+    #         return redirect("login")        
+    #     # auth.login(request, user)
+    #     # messages.success(request, "You are now logged in.")
+    #     # return redirect("home")
     return render(request, 'login.html')
 
 def homePage(request):
